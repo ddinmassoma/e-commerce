dac375d8516125a5346879e4ac246a668c9d2c9c
Faycal88 <merabet.faycal@outlook.com>
2026-04-03 20:04:49 +0100

    send email using gmail App password

--- formulaire_contact.php
+++ formulaire_contact.php
@@ -1,3 +1,17 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+?>
+
+
+
 <form action="" method="post" class="contact">
     <label>Nom :</label>
     <input type="text" name="nom" required>
@@ -20,6 +34,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+
     $name = $_POST['nom'];
     $surname = $_POST['prenom'];
     $email = $_POST['email'];
@@ -28,7 +43,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($name) || empty($surname) || empty($email) || empty($message) || empty($subject)) {
         echo "Tous les champs sont requis.";
+
     } else {
+
+        $mail = new PHPMailer(true);
+
+        try {
+            
+        // SMTP config
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'ddinmassoma@gmail.com';
+        $mail->Password   = 'slpz zqni agkf huhp';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+
+
+         // Sender & receiver
+        $mail->setFrom('ddinmassoma@gmail.com', 'Contact Website E-Commerce');
+        $mail->addAddress('ddinmassoma@gmail.com');
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+
+        
+        /* @TODO : Add HTML Template & CSS */
+         $mail->Body = "
+            <h3>Nouveau message</h3>
+            <p><strong>Nom:</strong> $name</p>
+            <p><strong>Prénom:</strong> $surname</p>
+            <p><strong>Email:</strong> $email</p>
+            <p><strong>Message:</strong><br>$message</p>
+        ";
+
+
+        $mail->AltBody = "Nom: $name\nPrenom: $surname\nEmail: $email\nMessage: $message";
+
+        $mail->send();
+
+        echo "Message envoyé avec succès ✅";
+
+
+
+        } catch (Exception $e) {
+             echo "Erreur: {$mail->ErrorInfo}";
+        }
 
     }
 }
